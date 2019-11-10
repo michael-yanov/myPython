516d6cbfc09386bcfee913f15c23089dc89190a0
Mike Yanov <mixa.odessa@gmail.com>
2019-11-11 01:10:51 +0200

    commit

---
+++ LogicalOperators.py
@@ -0,0 +1,3 @@
+x = int(input('First number is: '))
+y = int(input('Second number is: '))
+print(x > y)
---
+++ if_else.py
@@ -0,0 +1,11 @@
+# x = input('Input something here ---> ')
+# if x:
+#     print('OK')
+
+print('-----------////////////-------------')
+
+y = int(input('Please input number here: '))
+if y > 0:
+    print(1)
+else:
+    print(-1)
