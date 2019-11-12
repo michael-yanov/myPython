625e66fe93ee78c5683d327aed686dd1fc65b1c4
Mike <yanov.m.v@gmail.com>
2019-11-12 11:02:13 +0200

    commit

--- try_except.py
+++ try_except.py
@@ -1,19 +1,15 @@
 a = input('enter the first digit here - ')
 b = input('enter the second digit here - ')
-try:
-    if (a != '') and (b != ''):
-        a = float(a)
-        b = float(b)
-        c = input('type action here - ')
-        if c == '+':
-            print(a + b)
-        elif c == '-':
-            print(a - b)
-        elif c == '*':
-            print(a * b)
-        elif c == '/':
-            print(a / b)
-except TypeError:
-    print('you need to enter two digits')
-except ZeroDivisionError:
-    print('you can`t type zero')
+if (a != '') and (b != ''):
+    try:
+            a = float(a)
+            b = float(b)
+            c = input('enter action here - ')
+            if c == '+': print(a + b)
+            elif c == '-': print(a - b)
+            elif c == '*': print(a * b)
+            elif c == '/': print(a / b)
+    except ZeroDivisionError:
+        print('ERROR')
+else:
+    print('ERROR, please enter two digits')
