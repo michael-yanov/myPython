a046b5d7a036deafb4c68d619871ca9212b4f884
Mike <yanov.m.v@gmail.com>
2019-11-26 22:02:23 +0200

    first hillel commit

---
+++ hillel/lesson_1/escape_symbols.py
@@ -0,0 +1,14 @@
+'''
+Table of ESCAPE symbols
+'''
+
+print(
+    'Escape sequences\n'
+    '\\a - Bell (alert)\n',
+    '\r\\b - Backspace\n',
+    '\r\\n - New line\n',
+    '\r\\t - Horizontal tab\n',
+    '\r\\\ - Backslash \\\n'
+    '\r\\" - Double quotation mark \"\n'
+    '\r\\\' - Single quotation mark \'\n'
+)
---
+++ hillel/lesson_1/my_name.py
@@ -0,0 +1,12 @@
+'''
+Print name with ESCAPE symbols
+'''
+print(
+      '*\t\t*  *  *\t\t*  * * * * *\n',
+      '\r**     **  *  *    *   *\n',
+      '\r* *   * *  *  *   *    *\n',
+      '\r*\t*\t*  *  * *\t   * * * * *\n',
+      '\r*\t\t*  *  *   *\t   *\n',
+      '\r*\t\t*  *  *\t\t*  * \n'
+      '\r*\t\t*  *  *\t\t * * * * * *'
+      )
